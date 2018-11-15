f812d97c36f5868b799e37d426186561ad1de713
Asraful Khan <asrafulkhan@MacBook-Pro-3.local>
2018-11-15 18:20:21 -0500

    adding Q2 and updating Q1

--- src/Basic_Java/Q1/Q1.java
+++ src/Basic_Java/Q1/Q1.java
@@ -1,4 +1,4 @@
-package Basic_Java;
+package Basic_Java.Q1;
 
 /**
  * 1. Write a Java program to print 'Hello' on screen and then print your name on a separate line. Go to the editor
---
+++ src/Basic_Java/Q2/Q2.java
@@ -0,0 +1,21 @@
+package Basic_Java.Q2;
+
+/**
+ * 2. Write a Java program to print the sum of two numbers. Go to the editor
+ * Test Data:
+ * 74 + 36
+ * Expected Output :
+ * 110
+ */
+public class Q2 {
+
+    public static void main(String[] args) {
+
+
+    int sumOf, num1 = 74,  num2 = 36;
+        sumOf = num1 + num2;
+
+        System.out.println("Total " + sumOf);
+    }
+
+}
