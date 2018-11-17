2e29cf9c1c54827916cac2226df90ee2cfe453cc
Asraful Khan <asrafulkhan@MacBook-Pro-3.home>
2018-11-16 19:46:04 -0500

    adding Q2 and updating Q1

---
+++ src/Basic_Java/Q3/Q3.java
@@ -0,0 +1,21 @@
+package Basic_Java.Q3;
+
+public class Q3 {
+    public static void main(String[] args) {
+
+        /**
+         *
+         * 3. Write a Java program to divide two numbers and print on the screen. Go to the editor
+         * Test Data :
+         * 50/3
+         * Expected Output :
+         * 16
+         *
+         */
+
+        int div, num1 = 50, num2 = 3;
+        div = num1/num2;
+        System.out.println("Total " + div );
+
+    }
+}
