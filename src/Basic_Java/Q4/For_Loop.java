a8cbaa437f6b275ff9eb85dcedcbc0551a21e255
Asraful Khan <asrafulkhan@MacBook-Pro-3.local>
2018-11-19 16:27:28 -0500

    Adding Q4

---
+++ src/Basic_Java/Q4/For_Loop.java
@@ -0,0 +1,14 @@
+package Basic_Java.Q4;
+
+public class For_Loop {
+
+    public static void main(String[] args) {
+
+    for(int i=2;i <= 100;i=i+2 )
+
+        System.out.println(i);
+
+
+
+    }
+}
---
+++ src/Basic_Java/Q4/Q5.java
@@ -0,0 +1,19 @@
+package Basic_Java.Q4;
+
+public class Q5 {
+    public static void main(String[] args) {
+
+        /**
+         * 5. Write a Java program that takes two numbers as input and display the product of two numbers. Go to the editor
+         * Test Data:
+         * Input first number: 25
+         * Input second number: 5
+         * Expected Output :
+         * 25 x 5 = 125
+          */
+
+       int x= 25 ;
+       int Y= 5;
+       System.out.println(x*Y);
+    }
+}
